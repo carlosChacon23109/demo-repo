e8927348202e7f5ffc37487c48eb558b29441d1c
carlos <carlos.pruebas@hotmail.com>
2018-12-09 15:58:13 -0500

    se realiza una validacion de suma

--- class_toolsValidate.js
+++ class_toolsValidate.js
@@ -1,1 +1,16 @@
 ////////codigo de la raman nueva funcionalidad/////////////////
+function captacion_data(num1,num2){
+	if(isNaN(num1,num2){
+		return 1;
+	}
+	return 2;
+};
+
+////////SOLO REALIZA LA SUMA CON NUMEROS NO VALIDA CON LETRAS//////////
+
+	if(captacion_data == 1){
+		var suma = toInt(num1+num2)		
+	}	else{
+			alert("no se puede ejecutar la validacion");
+		}
+		
